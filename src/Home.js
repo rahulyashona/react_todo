e142278a3458c0618221e32c810d49c05e73da3c
Rahul Singh Yashona <rahulsinghyashona625@gmail.com>
2021-02-15 23:05:26 +0530

    second commit

---
+++ src/Home.js
@@ -0,0 +1,9 @@
+const Home = () => {
+    return ( 
+        <div className="Homepage">
+            <h1>Welcome to HomePage</h1>
+        </div>
+    );
+}
+ 
+export default Home;
---
+++ src/navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <p>The Bolg-Job</p>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/create">New Blog</a>
+            </div>
+        </nav>
+    );
+}
+ 
+export default Navbar;
